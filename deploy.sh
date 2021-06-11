5c5ab8e08c62293918c3593ac62c030f0d6c611f
linhnt169 <v.linhnt169@vinsmart.net.vn>
2021-06-11 16:35:47 +0700

    fix branch

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:nguyentienlinh2611/nguyentienlinh2611.github.io.git master
+git push -f git@github.com:nguyentienlinh2611/nguyentienlinh2611.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
